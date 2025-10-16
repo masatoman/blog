be8826b9edca4f31c459205455f81e309983bfa0
masatoman <kanon02sky@gmail.com>
2025-10-16 18:19:51 +0900

    Fix Astro config for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://masatoman.github.io',
   base: '/blog',
+  output: 'static',
   integrations: [tailwind()]
 });
